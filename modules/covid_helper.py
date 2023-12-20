5e1e08c6dbab981d6f683ccadae9718059b36a8a
Arda Sönmez <ssonmezarda@gmaik.com>
2023-12-20 20:49:13 +0300

    getDateInterval now faster

--- modules/covid_helper.py
+++ modules/covid_helper.py
@@ -6,6 +6,7 @@ import requests
 import asyncio
 import datetime
 import os
+import time
 
 class CovidConstants:
     allDataUrl = "https://covid19.who.int/WHO-COVID-19-global-data.csv"
@@ -24,9 +25,9 @@ class CovidHelper:
                 self.loadGlobalData()
             return function(self, *args, **kwargs)
         return wrapper
-    
+
     def loadGlobalData(self, reDownload=False) -> None:
-        if not reDownload: 
+        if not reDownload:
             self.global_data = CovidHelper.getCSV(CovidConstants.allDataName)
         else:
             CovidHelper.downloadData(CovidConstants.allDataUrl, CovidConstants.allDataName)
@@ -37,20 +38,20 @@ class CovidHelper:
         return self.global_data
 
     @checkGlobalData
-    def getCountryList(self) -> pd.DataFrame:
-        return self.global_data["Country"].drop_duplicates().reset_index()
-    
+    def getCountryList(self, dropIndex:bool=True) -> pd.DataFrame:
+        return self.global_data["Country"].drop_duplicates().reset_index(drop=dropIndex)
+
     @checkGlobalData
     def getCountryData(self, country:str, dropIndex:bool=True) -> pd.DataFrame:
         return self.global_data[self.global_data["Country"]==country].reset_index(drop=dropIndex)
-    
+
     @checkGlobalData
     def getDateData(self, date:str|datetime.date, dropIndex:bool=True) -> pd.DataFrame:
         if type(date) == datetime.date:
             return self.global_data[self.global_data["Date_reported"] == str(date)].reset_index(drop=dropIndex)
         elif type(date) == str:
             return self.global_data[self.global_data["Date_reported"] == date].reset_index(drop=dropIndex)
-    
+
     @checkGlobalData
     def getCountryDateData(self, date:str|datetime.date, country:str, dropIndex:bool=True) -> pd.DataFrame:
         global_data = self.global_data
@@ -59,12 +60,29 @@ class CovidHelper:
             ].reset_index(drop=dropIndex)
 
     @checkGlobalData
-    def getDateIntervalbyCountry(self, startDate:str|datetime.date, endDate:str|datetime.date, country:str, dropIndex:bool=True):
-        countryData = self.getCountryData(country, dropIndex=False)
+    def getDateIntervalbyCountry(self, startDate:str|datetime.date, endDate:str|datetime.date, country:str|list[str]="ALL", dropIndex:bool=True):
+        if type(country) == str and country == "ALL":
+            return self.global_data.query(f"Date_reported >= '{str(startDate)}'").query(f"Date_reported <= '{str(endDate)}'")
 
-        firstDateIndex = countryData[countryData["Date_reported"] == str(startDate)].iloc[0]["index"]
-        lastDateIndex = countryData[countryData["Date_reported"] == str(endDate)].iloc[0]["index"]
-        return self.global_data.loc[firstDateIndex:lastDateIndex]
+        if type(country) == str:
+            countryData = self.getCountryData(country, dropIndex=False)
+            if countryData.empty:
+                return countryData
+            
+            return countryData.query(f"Date_reported >= '{str(startDate)}'").query(f"Date_reported <= '{str(endDate)}'").reset_index(drop=dropIndex)
+        
+        if type(country) == list:
+            df = pd.DataFrame()
+            for c in country:
+                countryData = self.getCountryData(c, dropIndex=False)
+                if countryData.empty:
+                    continue
+                firstDateIndex = countryData[countryData["Date_reported"] == str(startDate)].iloc[0]["index"]
+                lastDateIndex = countryData[countryData["Date_reported"] == str(endDate)].iloc[0]["index"]
+                frames = [df, self.global_data.loc[firstDateIndex:lastDateIndex]]
+                df = pd.concat(frames)
+            return df
+
 
     @staticmethod
     def downloadData(url:str, path:str) -> bool:
@@ -77,14 +95,14 @@ class CovidHelper:
         print("Download Successful. File Saving...")
         with open(path, mode="wb") as file:
             file.write(response.content)
-        
+
         return True
 
     @staticmethod
-    def getCSV(path,url=CovidConstants.allDataUrl) -> pd.DataFrame:
+    def getCSV(path, url=CovidConstants.allDataUrl) -> pd.DataFrame:
         if os.path.exists(path):
             return pd.read_csv(path)
-        
+
         res = CovidHelper.downloadData(url, path)
         if res:
             return pd.read_csv(path)
@@ -99,9 +117,15 @@ class CovidHelper:
 def test():
     CovC = CovidConstants
     cHelper = CovidHelper()
+    print("Loading...")
+    st = time.time()
+    df = cHelper.getDateIntervalbyCountry(datetime.date(2022, 10, 1), datetime.date(2022, 11, 1), "Yemen")
+    ft = time.time()
+    print(f"Time1: {ft-st}")
+    print(df)
 
-    d = cHelper.getDateIntervalbyCountry(datetime.date(2022, 10, 1), datetime.date(2022, 11, 1), "Türkiye")
-    print(d)
+
+    #print(d)
 
 
 if __name__ == "__main__":
